2de81cc1ba2da4f58ca35040bbabcaca325bbf91
ShengBin-101 <shengbin.chan@gmail.com>
2025-03-15 10:07:18 +0800

    Add handling of unknown commands and exit command

--- src/main/java/seedu/bookKeeper/InputHandler.java
+++ src/main/java/seedu/bookKeeper/InputHandler.java
@@ -1,6 +1,5 @@
 package seedu.bookKeeper;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputHandler {
@@ -13,11 +12,10 @@ public class InputHandler {
     }
 
     public void askInput() {
-        // Add Implementation
         boolean isAskingInput = true;
         String userInputLine;
         Scanner scanner = new Scanner(System.in);
-    
+
         while (isAskingInput) {
             if (!scanner.hasNextLine()) {  // Prevents NoSuchElementException
                 break;
@@ -26,26 +24,34 @@ public class InputHandler {
             if (userInputLine.isEmpty()) {
                 System.out.println("Please enter a command");
             } else {
-                String[] commandArgs = InputParser.extractCommandArgs(userInputLine);
-                switch (commandArgs[0]) {
-                case "add-book":
-                    bookList.addBook(new Book(commandArgs[1],commandArgs[2], commandArgs[3], commandArgs[4]));
-                    break;
-                case "view-inventory":
-                    bookList.viewBookList();
-                    break;
-                case "remove-book":
-                    break;
-                case "add-loan":
-                    break;
-                case "delete-loan":
-                    break;
-                case "view-loans":
-                    break;
-                default:
-                    break;
+                try {
+                    String[] commandArgs = InputParser.extractCommandArgs(userInputLine);
+                    switch (commandArgs[0]) {
+                    case "add-book":
+                        bookList.addBook(new Book(commandArgs[1], commandArgs[2], commandArgs[3], commandArgs[4]));
+                        break;
+                    case "view-inventory":
+                        bookList.viewBookList();
+                        break;
+                    case "remove-book":
+                        break;
+                    case "add-loan":
+                        break;
+                    case "delete-loan":
+                        break;
+                    case "view-loans":
+                        break;
+                    case "exit":
+                        System.out.println("Exiting BookKeeper...");
+                        isAskingInput = false;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown command: " + commandArgs[0]);
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
             }
-        }   	
+        }
     }
 }
--- src/main/java/seedu/bookKeeper/InputParser.java
+++ src/main/java/seedu/bookKeeper/InputParser.java
@@ -1,28 +1,9 @@
 package seedu.bookKeeper;
+
 public class InputParser {
 
-    public static String[] extractCommandArgs(String input) {
-        String[] commandArgs = input.strip().split(" ", 2); 
-        switch (commandArgs[0]) {
-            case "add-book":
-                return extractAddBook(commandArgs[1]); // commandArgs[1] is the input that needs to be parsed (without the command)
-            case "view-inventory":
-                return commandArgs;
-            case "remove-book":
-                break;
-            case "add-loan":
-                break;
-            case "delete-loan":
-                break;
-            case "view-loans":
-                break;
-            default:
-                break;
-            }
-        // Add Implementation
-        // First parse to find out which command, then call the parser for that command
-        // Return String Array with commandArgs[0] being the command, followed by necessary parts to the command
-        return null;
+    public static String[] extractCommandArgs(String input) throws IllegalArgumentException {
+        return input.strip().split(" ", 2);
     }
 
     public static String[] extractXXXX(String input) {
@@ -40,7 +21,7 @@ public class InputParser {
         }
 
         commandArgs[0] = "add-book";
-        commandArgs[1] = splitInput[0]; 
+        commandArgs[1] = splitInput[0];
         commandArgs[2] = splitInput[1];
         commandArgs[3] = splitInput[2];
         commandArgs[4] = splitInput[3];
