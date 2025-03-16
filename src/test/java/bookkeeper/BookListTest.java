6ebd53311820c0aa53ae11fe1969d5e356e2194c
Phua-Lock-Hian <e1157347@u.nus.edu>
2025-03-16 18:24:52 +0800

    Amendments to pass Checkstyle tests

--- src/test/java/bookkeeper/BookListTest.java
+++ src/test/java/bookkeeper/BookListTest.java
@@ -2,7 +2,11 @@ package bookkeeper;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
